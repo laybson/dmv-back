4489a6999af185315517f15c1e510180121a2149
laybson <laybson.cunha@ccc.ufcg.edu.br>
2018-11-20 01:22:12 -0300

    login logic

--- elephas/config/keys.js
+++ elephas/config/keys.js
@@ -1,3 +1,4 @@
 module.exports = {
-  mongoURI: 'mongodb://laybson:laybson77@ds145355.mlab.com:45355/elephas'
+  mongoURI: 'mongodb://laybson:laybson77@ds145355.mlab.com:45355/elephas',
+  secretOrKey: 'secret'
 };
