f50e16181ba906db7a4549807eb613d1cddc9b06
laybson <laybson.cunha@ccc.ufcg.edu.br>
2018-11-25 20:38:57 -0300

    Add redux

--- elephas/client/src/components/auth/Login.js
+++ elephas/client/src/components/auth/Login.js
@@ -1,4 +1,8 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import classnames from 'classnames';
+import { loginUser } from '../../rActions/authActions';
 
 class Login extends Component {
   constructor() {
@@ -12,20 +16,36 @@ class Login extends Component {
     this.onSubmit = this.onSubmit.bind(this);
   }
 
+  componentDidMount() {
+    if (this.props.auth.isAuthenticated) {
+      this.props.history.push('/dashboard')
+    }
+  }
+
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.auth.isAuthenticated) {
+      this.props.history.push('/dashboard');
+    }
+    if (nextProps.errors) {
+      this.setState({ errors: nextProps.errors })
+    }
+  }
+
   onChange(e) {
     this.setState({[e.target.name]: e.target.value});
   }
 
   onSubmit(e) {
     e.preventDefault();
-    const loginUser = {
+    const userData = {
       email: this.state.email,
       password: this.state.password
     }
-    console.log(loginUser);
+    this.props.loginUser(userData);
   }
-  
+
   render() {
+    const { errors } = this.state;
     return (
       <div className="login">
         <div className="container">
@@ -37,20 +57,26 @@ class Login extends Component {
                 <div className="form-group">
                   <input
                     type="email"
-                    className="form-control form-control-lg"
+                    className={classnames('form-control form-control-lg',{
+                      'is-invalid': errors.email
+                    })}
                     placeholder="Email Address"
                     name="email"
                     value={ this.state.name }
-                    onChange={ this.onChange }/>
+                    onChange={ this.onChange } />
+                  { errors.email && (<div className="invalid-feedback">{ errors.email }</div>) }
                 </div>
                 <div className="form-group">
                   <input
                     type="password"
-                    className="form-control form-control-lg"
+                    className={classnames('form-control form-control-lg',{
+                      'is-invalid': errors.password
+                    })}
                     placeholder="Password"
                     name="password"
                     value={ this.state.password }
-                    onChange={ this.onChange }/>
+                    onChange={ this.onChange } />
+                  { errors.password && (<div className="invalid-feedback">{ errors.password }</div>) }
                 </div>
                 <input
                   type="submit"
@@ -64,4 +90,15 @@ class Login extends Component {
   }
 }
 
-export default Login;
+Login.propTypes = {
+  loginUser: PropTypes.func.isRequired,
+  auth: PropTypes.object.isRequired,
+  errors: PropTypes.object.isRequired
+}
+
+const mapStateToProps = (state) => ({
+  auth: state.auth,
+  errors: state.errors
+});
+
+export default connect(mapStateToProps, { loginUser })(Login);
--- elephas/client/src/components/layout/Navbar.js
+++ elephas/client/src/components/layout/Navbar.js
@@ -1,8 +1,52 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { logoutUser } from '../../rActions/authActions';
 
 class Navbar extends Component {
+  onLogoutClick(e) {
+    e.preventDefault();
+    this.props.logoutUser();
+  }
   render() {
+    const { isAuthenticated, user } = this.props.auth;
+    const authLinks = (
+      <div className="collapse navbar-collapse" id="mobile-nav">
+        <ul className="navbar-nav mr-auto">
+          <li className="nav-item">
+            <Link className="nav-link" to="/personas">Personas</Link>
+          </li>
+        </ul>
+        <ul className="navbar-nav ml-auto">
+          <li className="nav-item">
+            <a className="nav-link" href="" onClick={this.onLogoutClick.bind(this)}>
+              <img
+                className="rounded-circle"
+                src={ user.avatar }
+                alt={ user.name }
+                style={{ width: '25px', marginRight: '5px' }}
+                title="You must have a gravatar connected to your email to display an image"
+                />
+              {' '}Logout
+            </a>
+          </li>
+        </ul>
+      </div>
+    )
+
+    const guestLinks = (
+      <div className="collapse navbar-collapse" id="mobile-nav">
+        <ul className="navbar-nav ml-auto">
+          <li className="nav-item">
+            <Link className="nav-link" to="/register">Sign Up</Link>
+          </li>
+          <li className="nav-item">
+            <Link className="nav-link" to="/login">Login</Link>
+          </li>
+        </ul>
+      </div>
+    )
     return (
       <nav className="navbar navbar-expand-sm navbar-dark bg-dark mb-4">
         <div className="container">
@@ -11,26 +55,21 @@ class Navbar extends Component {
             <span className="navbar-toggler-icon"></span>
           </button>
 
-          <div className="collapse navbar-collapse" id="mobile-nav">
-            <ul className="navbar-nav mr-auto">
-              <li className="nav-item">
-                <Link className="nav-link" to="/personas">Personas</Link>
-              </li>
-            </ul>
+          { isAuthenticated ? authLinks : guestLinks }
 
-            <ul className="navbar-nav ml-auto">
-              <li className="nav-item">
-                <Link className="nav-link" to="/register">Sign Up</Link>
-              </li>
-              <li className="nav-item">
-                <Link className="nav-link" to="/login">Login</Link>
-              </li>
-            </ul>
-          </div>
         </div>
       </nav>
     )
   }
 }
 
-export default Navbar;
+Navbar.propTypes = {
+  logoutUser: PropTypes.func.isRequired,
+  auth: PropTypes.object.isRequired
+}
+
+const mapStateToProps = (state) => ({
+  auth: state.auth
+})
+
+export default connect(mapStateToProps, { logoutUser })(Navbar);
